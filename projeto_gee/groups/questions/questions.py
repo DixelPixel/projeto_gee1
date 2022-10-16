a8f75cb5289c7d1aff04cdf694834fae9bf9a973
JP Fernandes <joaopedro.jpbf@gmail.com>
2022-10-16 16:03:54 -0300

    Resolvendo modelos e relações

--- projeto_gee/groups/questions/questions.py
+++ projeto_gee/groups/questions/questions.py
@@ -4,10 +4,10 @@ from django.db import models
 from ..model.student import Student
 
 
-
 class Question(models.Model):
     name = models.CharField(max_length=200)
-    statement = models.CharField(max_length=2000,blank=True)
+    statement = models.CharField(max_length=2000, blank=True)
+    student = models.ManyToManyField(Student, blank=True)
 
     def __str__(self):
         return self.name
--- projeto_gee/groups/questions/tags.py
+++ projeto_gee/groups/questions/tags.py
@@ -1,17 +1,16 @@
 from ast import Sub
 from django.db import models
 
-from ..model.student import *
+from ..model.student import Student
 from .questions import Question
 from .study_method import Study_method
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=200)
-
-    student = models.ForeignKey(Student, blank=True,on_delete=models.CASCADE)
-    question = models.ManyToManyField(Question, blank=True)
-    study_method = models.ManyToManyField(Study_method, blank=True)
+    student = models.ManyToManyField(Student, blank=True, null=True)
+    question = models.ManyToManyField(Question, blank=True, null=True)
+    study_method = models.ManyToManyField(Study_method, blank=True, null=True)
 
     def __str__(self):
         return self.name
